379631352bedf4c091f8ebc4cc18974d887f2806
floodpeak <hongfengzhou@yahoo.com>
2016-12-07 00:00:21 +0800

    rf, combine the condition of 2,3,5

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,11 +1,8 @@
 module.exports = function(number){
-  if(number === 5){
-    return [5]
-  }
   if(number === 4){
     return [2,2]
   }
-  if(number === 2 || number === 3){
+  if(number === 2 || number === 3||number === 5){
     return [number]
   }
   return []
